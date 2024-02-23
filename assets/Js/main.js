e653a5224bb53d0db6ea236e7880abe042247dad
Kudae Sithu <kudaesithu2@gmail.com>
2024-02-23 16:08:01 +0630

    Mobile menu & animate navigation

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -72,11 +72,11 @@ const mobileSlideClose = document.querySelector('#mobile-slide-close');
 
 
 mobileMenu.addEventListener('click', () => {
-    mobileSlide.style.left = '0px'
+    mobileSlide.style.right = '0px'
 })
 
 mobileSlideClose.addEventListener('click', () => {
-    mobileSlide.style.left = '-100%'
+    mobileSlide.style.right = '-100%'
 })
 
 
@@ -87,73 +87,20 @@ var waypoint = new Waypoint({
     element: document.getElementById('chat'),
     handler: function(direction) {
         if(direction === "down") {
-            navControl.classList.add('fixed', 'w-full', 'shadow', 'animate__fadeInDown')
+            navControl.classList.add('fixed','w-full','shadow', 'animate__fadeInDown')
         } else {
-            navControl.classList.remove('fixed', 'w-full', 'shadow', 'animate__fadeInDown')
-        }
+            navControl.classList.remove('fixed', 'w-full','shadow', 'animate__fadeInUp')
+        }       
     },
-    offset: '75%'
+    offset: '10%'
   })
 
 
 //   Mobile-Slide-Closed
 let mobileSlideClosed = document.querySelectorAll('.mobile-slide-closed')
-
 for (let i = 0; i < mobileSlideClosed.length; i++) {
     // console.log(mobileSlideClosed[i])
     mobileSlideClosed[i].addEventListener('click', () => {
-        mobileSlide.style.left = '-100%'
+        mobileSlide.style.right = '-100%'
     })
 }
-
-// Nav Scroll Active link
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-// Document ထဲမှာရှိသမျှ section အားလုံးကို select လုပ်ထားပါတယ်။
-const sections = document.querySelectorAll("section[id]");
-
-function scrollActive() {
-  // Browser ကို Scroll ဆွဲလိုက်တဲ့အခါ Scroll ရောက်နေတဲ့ Height ကိုရရှိနေမှာဖြစ်ပါတယ်။
-  const scrollY = window.pageYOffset; // scroll height
-//   console.log(scrollY)
-
-  // forEach နဲ့ ရှိသမျှ section အကုန်လုံးကို loop လုပ်လိုက်တာဖြစ်ပါတယ်။
-  // current.offsetHeight လက်ရှိရောက်နေတဲ့ section ရဲ့ Height ကို ရယူလိုက်တာပါ။
-  // current.offsetTop - 58 လက်ရှိရောက်နေတဲ့ section ရဲ့ Top ကို ရယူလိုက်တာပါ။
-  // current.getAttribute("id") လက်ရှိရောက်နေတဲ့ section ရဲ့ id ကို ရယူလိုက်တာပါ။
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight, // get current height
-      sectionTop = current.offsetTop - 58, // get current section of height
-      sectionId = current.getAttribute("id"); // get current section id
-
-
-    // scrollyY သည် sectionTop ထက်ကြီးနေတယ်ဆိုရင် True အနေနဲ့ သတ်မှတ်မှာဖြစ်ပြီး
-    // လက်ရှိရောက်နေတဲ့ section ရဲ့ Top နဲ့ လက်ရှိရောက်နေတဲ့ section ရဲ့ Height နှစ်ခုကိုပေါင်းပြီး
-    // scrollyY သည် ငယ်နေတယ်(သို့) ညီနေတယ်ဆိုရင် True ကိုရရှိမှာပါ။
-    // && သည် Comparsion Operator ဖြစ်ပြီး နှင်းယှဉ်ထားတဲ့ တန်ဖိုးနှစ်ခုလုံး True && True ဖြစ်မှသာ
-    // if statement ကိုအလုပ်လုပ်မှာဖြစ်ပါတယ်။ နှိုင်းယှဉ်လိုက်တဲ့ တန်ဖိုးက false ဖြစ်နေလျှင် else ကို အလုပ်လုပ်သွားမှာဖြစ်ပါတယ်။
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector("a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector("a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
-}
-// function သည် ကြေညာလိုက်ရုံနဲ့ အလုပ်မလုပ်ပါဘူး။
-// သူ့ကို ပြန်ခေါ်သုံးမှသာ အလုပ်လုပ်တာဖြစ်ပါတယ်။
-// ဒါကြောင့် Browser ကို Scroll လုပ်လိုက်လျှင် scrollActive ကို အလုပ်လုပ်ပေးပါလို့ပြောလိုက်ခြင်းဖြစ်ပါတယ်။
-window.addEventListener("scroll", scrollActive);
-
-
-// ScrollReveal
-ScrollReveal().reveal('.headline', { 
-    delay: 300,
-    origin: 'bottom',
-    distance: '50px',
-    interval: 300,
-    scale: 0.85,
-    reset: true
-});
