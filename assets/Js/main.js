8b8e345b759aeade04862147a44019dd5731c236
Kudae Sithu <kudaesithu2@gmail.com>
2024-02-24 22:55:06 +0630

    Added loading

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -108,7 +108,6 @@ sections.forEach((current) => {
     const sectionHeight = current.offsetHeight, 
     sectionTop = current.offsetTop - 58, 
     sectionId = current.getAttribute("id"); 
-    console.log(sectionId);
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
